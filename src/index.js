f535e1fc11c928852bfd77d057d74c86c9dc0452
Mario Freire <huege.design@gmail.com>
2019-01-15 17:51:00 -0200

    DESAFIO ÚNICO: adiciona a frente do card com JS e CSS

--- src/index.js
+++ src/index.js
@@ -11,3 +11,20 @@ $memoryCard.classList.add("memory-card");
 
 $root.insertBefore($memoryCard, null);
 $memoryCard.insertAdjacentHTML("afterbegin", $icon);
+
+const $ball = document.createElement("div");
+const $memoryCardFront = document.createElement("article");
+const $iconActive = `
+    <img src='img/icon-c.png'
+    alt='linguagem c++'
+    class='icon -active'
+    />
+`;
+
+$ball.style.cssText =
+  "display: block; background-color: #d4d4d4; width: 95px; height: 95px; border-radius: 50%";
+
+$memoryCardFront.classList.add("memory-card", "-active");
+$memoryCardFront.insertAdjacentElement("afterbegin", $ball);
+$ball.insertAdjacentHTML("afterbegin", $iconActive);
+$root.insertBefore($memoryCardFront, null);
