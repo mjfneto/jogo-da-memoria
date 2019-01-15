28a4e405dcf6fcf876aec5be462937cb36aef8ee
Mario Freire <huege.design@gmail.com>
2019-01-15 15:54:09 -0200

    Notas e mods 021.008 "Sofrendo para aprender o insertAdjacentHTML"

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const $root = document.querySelector("#root");
 const $memoryCard = document.createElement("article");
+const $icon =
+  "<img src='img/icon-collabcode.svg' alt='Gueio, mascote da CollabCode' class='icon'/>";
 
 $memoryCard.classList.add("memory-card");
 
 $root.insertBefore($memoryCard, null);
+$memoryCard.insertAdjacentHTML("afterbegin", $icon);
