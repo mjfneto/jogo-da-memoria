f3df053f0e045fb12409f98a8bb22ab686cd88ec
Mario Freire <huege.design@gmail.com>
2019-02-27 20:24:01 -0300

    Notas e mods 035.03 "Deixando nossa cada top com css"

--- Aula35.md
+++ Aula35.md
@@ -3,3 +3,5 @@
 ## 01 "Criar object para nossa camada"
 
 ## 02 "Transparency-layer na nossa page feliz"
+
+## 03 "Deixando nossa cada top com css"
--- src/objects/transparency-layer/index.js
+++ src/objects/transparency-layer/index.js
@@ -2,12 +2,26 @@ const transparencyLayer = (function(){
     const module = {};
 
     module._style = () => {
-        
+        const $head = document.querySelector('head');
+        const $style = document.createElement('style');
+
+        $style.textContent = `
+            .transparency-layer {
+                display: block;
+                background-color: rgba(58, 64, 66, 0.5);
+                height: 100vh;
+            }
+        `;
+
+        $head.insertBefore($style, null);
     };
 
-    module.render = () => `
+    module.render = () => {
+        module._style();
+        return `
             <div class="transparency-layer"></div>
-    `;
+        `
+    };
 
     return {
         render: module.render
